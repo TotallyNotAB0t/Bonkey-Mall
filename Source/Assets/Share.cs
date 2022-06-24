4acfca4ad1d632402fc8b83b94f36409884fe756
Teyo01 <dubuissontheo@outlook.fr>
2022-06-24 10:44:13 +0200

    rs up

--- Source/Assets/Share.cs
+++ Source/Assets/Share.cs
@@ -6,11 +6,16 @@ public class Share : MonoBehaviour
     /*TWITTER*/
     string TWITTER_ADDRESS = "http://twitter.com/intent/tweet";
     string TWEET_LANGUAGE = "en";
-    string textToDisplay="Hey Guys! Check out my score: ";
+    string textToDisplay="Hey Guys! Check out my place on this race of Bonkey Mall : ";
 
     /*FACEBOOK*/
     string AppID = "553708503016668";
     string Link = "https://github.com/Chaussurre/Glitch-Racers/tree/NewProjectBranch";
+    string PictureFirst = "https://i.imgur.com/U72BfWR.png"; 
+    string PictureSecond = "https://i.imgur.com/8h74FZg.png";
+    string PictureThird = "https://i.imgur.com/4N9Itdo.png";
+    /* l'API de facebook ne permet plus de préremplir des post, ceux pourquoi nous avons opté pour cette solution qui est la meilleure pour le moment */
+    
     public void shareScoreOnTwitter () 
     {
         Application.OpenURL (TWITTER_ADDRESS + "?text=" + WWW.EscapeURL(textToDisplay) + LevelManager.GetPlace() + "&amp;lang=" + WWW.EscapeURL(TWEET_LANGUAGE));
@@ -20,6 +25,19 @@ public class Share : MonoBehaviour
     public void shareScoreOnFacebook ()
     {
         ScreenCapture.CaptureScreenshot("SomeLevel");
-        Application.OpenURL ("https://www.facebook.com/dialog/feed?" + "app_id=" + AppID + "&link=" + Link);
+        if (LevelManager.GetPlace() == 1)
+        {
+            Application.OpenURL ("https://www.facebook.com/dialog/feed?" + "app_id=" + AppID + "&picture=" + PictureFirst);
+        }
+
+        if (LevelManager.GetPlace() == 2)
+        {
+            Application.OpenURL ("https://www.facebook.com/dialog/feed?" + "app_id=" + AppID + "&picture=" + PictureSecond);
+        }
+
+        if (LevelManager.GetPlace() == 3)
+        {
+            Application.OpenURL ("https://www.facebook.com/dialog/feed?" + "app_id=" + AppID + "&picture=" + PictureThird);
+        }
     }
 }

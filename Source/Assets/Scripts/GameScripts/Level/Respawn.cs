9f4e3df69456754629cbd4a75716aacec4d79a92
Pierre Durand <85309955+TotallyNotAB0t@users.noreply.github.com>
2022-07-04 22:06:20 +0200

    Lvl1 reworked

--- Source/Assets/Scripts/GameScripts/Level/Respawn.cs
+++ Source/Assets/Scripts/GameScripts/Level/Respawn.cs
@@ -2,13 +2,12 @@ using UnityEngine;
 
 public class Respawn : MonoBehaviour
 {
-    [SerializeField] private Transform mole;
     [SerializeField] private Transform lastCP;
 
     private void OnCollisionEnter(Collision collision)
     {
-        TpObject(mole, lastCP);
-        ResetSpeed(mole.GetComponent<Rigidbody>());
+        TpObject(collision.gameObject.transform, lastCP);
+        ResetSpeed(collision.gameObject.GetComponent<Rigidbody>());
     }
 
     private static void TpObject(Transform mole, Transform CP)
@@ -19,10 +18,5 @@ public class Respawn : MonoBehaviour
     private void ResetSpeed(Rigidbody rig)
     {
         rig.velocity = Vector3.zero;
-    }
-
-    private void ChangeCp(Transform newCp)
-    {
-        lastCP = newCp;
     }
 }
--- Source/Assets/power/blockOthers.cs
+++ Source/Assets/power/blockOthers.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -11,18 +10,6 @@ public class blockOthers : MonoBehaviour
     [SerializeField] private GameObject bot1;
 
     [SerializeField] private GameObject bot2;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -56,20 +43,18 @@ public class blockOthers : MonoBehaviour
     private IEnumerator blockPlayerBot2()
     {
         player.GetComponent<Rigidbody>().mass = 10000f;
-        yield return new WaitForSeconds(3);
-        player.GetComponent<Rigidbody>().mass = 1;
         bot2.GetComponent<NavMeshAgent>().speed = 0;
         yield return new WaitForSeconds(3);
-        bot2.GetComponent<NavMeshAgent>().speed = 112;
+        player.GetComponent<Rigidbody>().mass = 1;
+        bot2.GetComponent<NavMeshAgent>().speed = 12;
     }
     
     private IEnumerator blockPlayerBot1()
     {
         player.GetComponent<Rigidbody>().mass = 10000f;
-        yield return new WaitForSeconds(3);
-        player.GetComponent<Rigidbody>().mass = 1;
         bot1.GetComponent<NavMeshAgent>().speed = 0;
         yield return new WaitForSeconds(3);
+        player.GetComponent<Rigidbody>().mass = 1;
         bot1.GetComponent<NavMeshAgent>().speed = 12;
     }
 }
